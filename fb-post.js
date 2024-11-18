283bba40d5a53443d9cd17693b12d800d6ad869d
Vic Chimenti <victor.chimenti@gmail.com>
2024-11-18 14:24:43 -0800

    seattleu~sp-search

---
+++ fb-post.js
@@ -0,0 +1,23 @@
+function performPostSearch(query) {
+    const formData = new FormData();
+    formData.append('query', query);
+    formData.append('collection', 'seattleu~sp-search');
+  
+    fetch('/s/search.json', {
+      method: 'POST',
+      body: formData
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        // Process the search results
+        console.log(data);
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  }
