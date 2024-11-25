2736cc9c8cc90d23709fea3f0b27add7a48880ee
Vic Chimenti <victor.chimenti@gmail.com>
2024-11-25 14:13:37 -0800

    getTabResponse

--- fb-partial-function-tabs.js
+++ fb-partial-function-tabs.js
@@ -78,6 +78,64 @@ async function fetchFunnelbackWithQuery(url, method, userIP, searchQuery) {
   }
 }
 
+
+// Funnelback fetch function
+async function fetchFunnelbackWithTabs(url, method, userIP, searchQuery) {
+
+  console.log("async method: " + method);
+  try {
+    if (method === 'GET' && searchQuery) {
+      url += `?query=${encodeURIComponent(searchQuery)}&collection=seattleu~sp-search&profile=_default&form=partial`;
+    }
+
+    let options = {
+      method,
+      headers: {
+        'Content-Type': method === 'POST' ? 'text/plain' : 'application/json',
+        // 'X-Forwarded-For': userIP,
+      },
+    };
+
+    let response = await fetch(url, options);
+    if (!response.ok) throw new Error(`Error: ${response.status}`);
+
+    console.log("response status: " + response.status);
+    console.log("response type: " + response.type);
+
+
+    let stream = response.body.pipeThrough(new TextDecoderStream());
+    let reader = stream.getReader();
+    let text = "";
+
+    try {
+      while (true) {
+        const { value, done } = await reader.read();
+
+        console.log("initial value: " + value);
+
+        if (done) {
+          console.log("Stream reading complete.");
+          break;
+        }
+        text += value;
+
+        console.log("text value:", + text); // Process each chunk of data
+      }
+
+    } catch (error) {
+      console.error("Error reading stream:", error);
+    } finally {
+      reader.releaseLock();
+    }
+  
+    return text;
+
+  } catch (error) {
+    console.error(`Error with ${method} request:`, error);
+    return `<p>Error fetching ${method} request. Please try again later.</p>`;
+  }
+}
+
 // Handle form submission
 searchBar.addEventListener('click', async (event) => {
   event.preventDefault(); // Prevent page reload
@@ -131,12 +189,12 @@ searchTab.addEventListener('click', async (event) => {
   console.log("Query: " + searchQuery);
 
   // Define Funnelback URLs
-  let getUrl = 'https://dxp-us-stage-search.funnelback.squiz.cloud/s/search.html';
-  // let getResponse = await (fetchFunnelbackWithQuery(getUrl, 'GET', userIP, searchQuery));
-  // console.log("getResponse: " + getResponse);
+  let getTabUrl = 'https://dxp-us-stage-search.funnelback.squiz.cloud/s/search.html';
+  let getTabResponse = await (fetchFunnelbackWithTabs(getTabUrl, 'GET', userIP, searchQuery, tabName));
+  console.log("getTabResponse: " + getTabResponse);
 
   // Display results
   document.getElementById('results').innerHTML = `
-    <div class="funnelback-search-container">${getResponse}</div>
+    <div class="funnelback-search-container">${getTabResponse}</div>
   `;
 });
