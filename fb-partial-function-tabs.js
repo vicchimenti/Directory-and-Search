97bb8cac14c52e162cf6794618258cee9d9f8e2f
Vic Chimenti <victor.chimenti@gmail.com>
2024-11-25 10:44:22 -0800

    rm cm cd post

--- fb-partial-function-tabs.js
+++ fb-partial-function-tabs.js
@@ -90,16 +90,8 @@ document.getElementById('search-button').addEventListener('click', async (event)
 
   // Define Funnelback URLs
   let getUrl = 'https://dxp-us-stage-search.funnelback.squiz.cloud/s/search.html';
-  // let postUrl = 'https://dxp-us-stage-search.funnelback.squiz.cloud/s/search.html?collection=seattleu~sp-search&profile=_default&form=partial';
-
   let getResponse = await (fetchFunnelbackWithQuery(getUrl, 'GET', userIP, searchQuery));
-
-  // let postResponse = await(fetchFunnelbackWithQuery(postUrl, 'POST', userIP, searchQuery));
-
-
   console.log("getResponse: " + getResponse);
-  // console.log("postResponse: " + postResponse);
-
 
   // Display results
   document.getElementById('results').innerHTML = `
