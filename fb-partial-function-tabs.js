76f2341f7b12250f3e0a6bb97404bc8dac8ce5be
Vic Chimenti <victor.chimenti@gmail.com>
2024-12-02 10:07:35 -0800

    ud uia assignmnet

--- fb-partial-function-tabs.js
+++ fb-partial-function-tabs.js
@@ -7,16 +7,16 @@ let userIpAddress = null;
 // Fetch user's IP address
 document.addEventListener('DOMContentLoaded', function() {
    async function getUserIP() {
-    try {
-      let response = await fetch('https://api.ipify.org?format=json');
-      let data = await response.json();
-      console.log("getIP: " + data.ip);
-      userIpAddress = JSON.stringify(data.ip);
-    } catch (error) {
-      console.error('Error fetching IP address:', error);
-      return ''; // Default to empty if error occurs
+      try {
+        let response = await fetch('https://api.ipify.org?format=json');
+        let data = await response.json();
+        console.log("getIP: " + data.ip);
+        userIpAddress = JSON.stringify(data.ip);
+      } catch (error) {
+          console.error('Error fetching IP address:', error);
+          // return ''; // Default to empty if error occurs
+      }
     }
-  }
 });
 
 // Funnelback fetch function
