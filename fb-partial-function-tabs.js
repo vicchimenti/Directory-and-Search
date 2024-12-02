3679f1987e7ac4b97fa9cdcee89a967293688cdd
Vic Chimenti <victor.chimenti@gmail.com>
2024-12-02 10:12:58 -0800

    ip method

--- fb-partial-function-tabs.js
+++ fb-partial-function-tabs.js
@@ -3,20 +3,22 @@ const searchBar = document.getElementById("search-button");
 let getResponse = null;
 let userIpAddress = null;
 
+async function getUserIP() {
+  try {
+    let response = await fetch('https://api.ipify.org?format=json');
+    let data = await response.json();
+    console.log("getIP: " + data.ip);
+    return data.ip;
+  } catch (error) {
+      console.error('Error fetching IP address:', error);
+      return ''; // Default to empty if error occurs
+  }
+}
 
 // Fetch user's IP address
-document.addEventListener('DOMContentLoaded', function() {
-   async function getUserIP() {
-      try {
-        let response = await fetch('https://api.ipify.org?format=json');
-        let data = await response.json();
-        console.log("getIP: " + data.ip);
-        userIpAddress = JSON.stringify(data.ip);
-      } catch (error) {
-          console.error('Error fetching IP address:', error);
-          // return ''; // Default to empty if error occurs
-      }
-    }
+document.addEventListener('DOMContentLoaded', async function() {
+  let userIp = await getUserIP();
+  userIpAddress = JSON.stringify(userIp);
 });
 
 // Funnelback fetch function
