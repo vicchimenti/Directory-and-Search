f4a324a50658a1fb0293432a80aed7e3fe42da0f
Vic Chimenti <victor.chimenti@gmail.com>
2024-12-15 15:25:48 -0800

    cm handle clicks and fetch legacy

--- fb-partial-function-banner-search.js
+++ fb-partial-function-banner-search.js
@@ -1,6 +1,6 @@
 // capture search bar and globaldeclarations
 const searchBar = document.getElementById("search-button");
-const onPageSearch = document.getElementById("on-page-search-button");
+// const onPageSearch = document.getElementById("on-page-search-button");
 let userIpAddress = null;
 let userIp = null;
 
@@ -83,354 +83,354 @@ async function fetchFunnelbackWithQuery(url, method, searchQuery) {
 
 
 // Funnelback fetch tabs function
-async function fetchFunnelbackResults(url, method) {
+// async function fetchFunnelbackResults(url, method) {
 
-  let prodTabUrl = 'https://dxp-us-search.funnelback.squiz.cloud/s/search.html';
+//   let prodTabUrl = 'https://dxp-us-search.funnelback.squiz.cloud/s/search.html';
 
-  try {
-    if (method === 'GET') {
-      prodTabUrl += `${url}`;
-    }
+//   try {
+//     if (method === 'GET') {
+//       prodTabUrl += `${url}`;
+//     }
 
-    let options = {
-      method,
-      headers: {
-        'Content-Type': method === 'POST' ? 'text/plain' : 'application/json',
-        // 'X-Forwarded-For': userIp,
-      },
-    };
+//     let options = {
+//       method,
+//       headers: {
+//         'Content-Type': method === 'POST' ? 'text/plain' : 'application/json',
+//         // 'X-Forwarded-For': userIp,
+//       },
+//     };
 
-    let response = await fetch(prodTabUrl, options);
-    if (!response.ok) throw new Error(`Error: ${response.status}`);
+//     let response = await fetch(prodTabUrl, options);
+//     if (!response.ok) throw new Error(`Error: ${response.status}`);
 
-    let stream = response.body.pipeThrough(new TextDecoderStream());
-    let reader = stream.getReader();
-    let text = "";
+//     let stream = response.body.pipeThrough(new TextDecoderStream());
+//     let reader = stream.getReader();
+//     let text = "";
 
-    try {
-      while (true) {
-        const { value, done } = await reader.read();
+//     try {
+//       while (true) {
+//         const { value, done } = await reader.read();
 
-        if (done) {
-          break;
-        }
-        text += value;
-      }
+//         if (done) {
+//           break;
+//         }
+//         text += value;
+//       }
 
-    } catch (error) {
-      console.error("Error reading stream:", error);
-    } finally {
-      reader.releaseLock();
-    }
+//     } catch (error) {
+//       console.error("Error reading stream:", error);
+//     } finally {
+//       reader.releaseLock();
+//     }
   
-    return text;
+//     return text;
 
-  } catch (error) {
-    console.error(`Error with ${method} request:`, error);
-    return `<p>Error fetching ${method} tabbed request. Please try again later.</p>`;
-  }
-}
+//   } catch (error) {
+//     console.error(`Error with ${method} request:`, error);
+//     return `<p>Error fetching ${method} tabbed request. Please try again later.</p>`;
+//   }
+// }
 
 
 
 
 // Funnelback fetch search tools function
-async function fetchFunnelbackTools(url, method) {
+// async function fetchFunnelbackTools(url, method) {
 
-  let prodToolsUrl = 'https://dxp-us-search.funnelback.squiz.cloud/s/';
+//   let prodToolsUrl = 'https://dxp-us-search.funnelback.squiz.cloud/s/';
 
-  try {
-    if (method === 'GET') {
-      prodToolsUrl += `${url}`;
-    }
+//   try {
+//     if (method === 'GET') {
+//       prodToolsUrl += `${url}`;
+//     }
 
-    let options = {
-      method,
-      headers: {
-        'Content-Type': method === 'POST' ? 'text/plain' : 'application/json',
-        // 'X-Forwarded-For': userIp,
-      },
-    };
+//     let options = {
+//       method,
+//       headers: {
+//         'Content-Type': method === 'POST' ? 'text/plain' : 'application/json',
+//         // 'X-Forwarded-For': userIp,
+//       },
+//     };
 
-    let response = await fetch(prodToolsUrl, options);
-    if (!response.ok) throw new Error(`Error: ${response.status}`);
+//     let response = await fetch(prodToolsUrl, options);
+//     if (!response.ok) throw new Error(`Error: ${response.status}`);
 
-    let stream = response.body.pipeThrough(new TextDecoderStream());
-    let reader = stream.getReader();
-    let text = "";
+//     let stream = response.body.pipeThrough(new TextDecoderStream());
+//     let reader = stream.getReader();
+//     let text = "";
 
-    try {
-      while (true) {
-        const { value, done } = await reader.read();
+//     try {
+//       while (true) {
+//         const { value, done } = await reader.read();
 
-        if (done) {
-          break;
-        }
-        text += value;
-      }
+//         if (done) {
+//           break;
+//         }
+//         text += value;
+//       }
 
-    } catch (error) {
-      console.error("Error reading stream:", error);
-    } finally {
-      reader.releaseLock();
-    }
+//     } catch (error) {
+//       console.error("Error reading stream:", error);
+//     } finally {
+//       reader.releaseLock();
+//     }
   
-    return text;
+//     return text;
 
-  } catch (error) {
-    console.error(`Error with ${method} request:`, error);
-    return `<p>Error fetching ${method} tabbed request. Please try again later.</p>`;
-  }
-}
+//   } catch (error) {
+//     console.error(`Error with ${method} request:`, error);
+//     return `<p>Error fetching ${method} tabbed request. Please try again later.</p>`;
+//   }
+// }
 
 
 
 
 // Funnelback fetch search tools function
-async function fetchFunnelbackSpelling(url, method) {
+// async function fetchFunnelbackSpelling(url, method) {
 
-  let prodSpellingUrl = 'https://dxp-us-search.funnelback.squiz.cloud/s/';
-  let partial = '&form=partial';
-  let query = url +=`${partial}`;
+//   let prodSpellingUrl = 'https://dxp-us-search.funnelback.squiz.cloud/s/';
+//   let partial = '&form=partial';
+//   let query = url +=`${partial}`;
 
-  try {
-    if (method === 'GET') {
-      prodSpellingUrl += `${query}`;
-    }
+//   try {
+//     if (method === 'GET') {
+//       prodSpellingUrl += `${query}`;
+//     }
 
-    let options = {
-      method,
-      headers: {
-        'Content-Type': method === 'POST' ? 'text/plain' : 'application/json',
-        // 'X-Forwarded-For': userIp,
-      },
-    };
+//     let options = {
+//       method,
+//       headers: {
+//         'Content-Type': method === 'POST' ? 'text/plain' : 'application/json',
+//         // 'X-Forwarded-For': userIp,
+//       },
+//     };
 
-    let response = await fetch(prodSpellingUrl, options);
-    if (!response.ok) throw new Error(`Error: ${response.status}`);
+//     let response = await fetch(prodSpellingUrl, options);
+//     if (!response.ok) throw new Error(`Error: ${response.status}`);
 
-    let stream = response.body.pipeThrough(new TextDecoderStream());
-    let reader = stream.getReader();
-    let text = "";
+//     let stream = response.body.pipeThrough(new TextDecoderStream());
+//     let reader = stream.getReader();
+//     let text = "";
 
-    try {
-      while (true) {
-        const { value, done } = await reader.read();
+//     try {
+//       while (true) {
+//         const { value, done } = await reader.read();
 
-        if (done) {
-          break;
-        }
-        text += value;
-      }
+//         if (done) {
+//           break;
+//         }
+//         text += value;
+//       }
 
-    } catch (error) {
-      console.error("Error reading stream:", error);
-    } finally {
-      reader.releaseLock();
-    }
+//     } catch (error) {
+//       console.error("Error reading stream:", error);
+//     } finally {
+//       reader.releaseLock();
+//     }
   
-    return text;
+//     return text;
 
-  } catch (error) {
-    console.error(`Error with ${method} request:`, error);
-    return `<p>Error fetching ${method} tabbed request. Please try again later.</p>`;
-  }
-}
+//   } catch (error) {
+//     console.error(`Error with ${method} request:`, error);
+//     return `<p>Error fetching ${method} tabbed request. Please try again later.</p>`;
+//   }
+// }
 
 
 
-// handle tab listeners
-async function handleTab(e) {
-  e.preventDefault();
+// // handle tab listeners
+// async function handleTab(e) {
+//   e.preventDefault();
 
-  const fetchTab = e.target.closest('.tab-list__nav a');
-  const tabHref = fetchTab.getAttribute('href');
-  console.log("Relative href:", tabHref);
+//   const fetchTab = e.target.closest('.tab-list__nav a');
+//   const tabHref = fetchTab.getAttribute('href');
+//   console.log("Relative href:", tabHref);
 
-  // Fetch and process data using the relative link
-  let getTabResponse = null;
-  if (tabHref) {
-    try {
-      getTabResponse = await fetchFunnelbackResults(tabHref, 'GET');
-    } catch (error) {
-      console.error("Error fetching tab data:", error);
-      getTabResponse = "Error loading tab results.";
-    }
-  }
+//   // Fetch and process data using the relative link
+//   let getTabResponse = null;
+//   if (tabHref) {
+//     try {
+//       getTabResponse = await fetchFunnelbackResults(tabHref, 'GET');
+//     } catch (error) {
+//       console.error("Error fetching tab data:", error);
+//       getTabResponse = "Error loading tab results.";
+//     }
+//   }
 
-  document.getElementById('results').innerHTML = `
-    <div class="funnelback-search-container">
-      ${getTabResponse || "No tab results found."}
-    </div>
-  `;
-}
+//   document.getElementById('results').innerHTML = `
+//     <div class="funnelback-search-container">
+//       ${getTabResponse || "No tab results found."}
+//     </div>
+//   `;
+// }
 
 
 
 
 // Function to handle anchor clicks
-async function handleFacetAnchor(e) {
-  e.preventDefault();
+// async function handleFacetAnchor(e) {
+//   e.preventDefault();
 
-  const facetAnchor = e.target.closest('.facet-group__list a');
-  const facetHref = facetAnchor.getAttribute('href');
-  console.log("Relative href:", facetHref);
+//   const facetAnchor = e.target.closest('.facet-group__list a');
+//   const facetHref = facetAnchor.getAttribute('href');
+//   console.log("Relative href:", facetHref);
 
-  // Fetch and process data using the relative link
-  let getFacetResponse = null;
-  if (facetHref) {
-    try {
-      getFacetResponse = await fetchFunnelbackResults(facetHref, 'GET');
-    } catch (error) {
-      console.error("Error fetching facet data:", error);
-      getFacetResponse = "Error loading facet results.";
-    }
-  }
+//   // Fetch and process data using the relative link
+//   let getFacetResponse = null;
+//   if (facetHref) {
+//     try {
+//       getFacetResponse = await fetchFunnelbackResults(facetHref, 'GET');
+//     } catch (error) {
+//       console.error("Error fetching facet data:", error);
+//       getFacetResponse = "Error loading facet results.";
+//     }
+//   }
 
-  document.getElementById('results').innerHTML = `
-    <div class="funnelback-search-container">
-      ${getFacetResponse || "No facet results found."}
-    </div>
-  `;
-}
+//   document.getElementById('results').innerHTML = `
+//     <div class="funnelback-search-container">
+//       ${getFacetResponse || "No facet results found."}
+//     </div>
+//   `;
+// }
 
 
 
 
 // handle search tool listeners
-async function handleSearchTools(e) {
-  e.preventDefault();
+// async function handleSearchTools(e) {
+//   e.preventDefault();
 
-  const fetchTools = e.target.closest('.search-tools__button-group a');
-  const toolHref = fetchTools.getAttribute('href');
-  console.log("Relative href:", toolHref);
+//   const fetchTools = e.target.closest('.search-tools__button-group a');
+//   const toolHref = fetchTools.getAttribute('href');
+//   console.log("Relative href:", toolHref);
 
-  // Fetch and process data using the relative link
-  let getToolResponse = null;
-  if (toolHref) {
-    try {
-      getToolResponse = await fetchFunnelbackTools(toolHref, 'GET');
-    } catch (error) {
-      console.error("Error fetching tab data:", error);
-      getToolResponse = "Error loading tool results.";
-    }
-  }
+//   // Fetch and process data using the relative link
+//   let getToolResponse = null;
+//   if (toolHref) {
+//     try {
+//       getToolResponse = await fetchFunnelbackTools(toolHref, 'GET');
+//     } catch (error) {
+//       console.error("Error fetching tab data:", error);
+//       getToolResponse = "Error loading tool results.";
+//     }
+//   }
 
-  document.getElementById('results').innerHTML = `
-    <div class="funnelback-search-container">
-      ${getToolResponse || "No tool results found."}
-    </div>
-  `;
-}
-
-
-
-
-// handle facet cleaners
-async function handleClearFacet(e) {
-  e.preventDefault();
-
-  const fetchClear = e.target.closest('a.facet-group__clear');
-  const clearHref = fetchClear.getAttribute('href');
-  console.log("Relative href:", clearHref);
-
-  // Fetch and process data using the relative link
-  let getClearResponse = null;
-  if (clearHref) {
-    try {
-      getClearResponse = await fetchFunnelbackResults(clearHref, 'GET');
-    } catch (error) {
-      console.error("Error fetching clear data:", error);
-      getClearResponse = "Error loading clear results.";
-    }
-  }
-
-  document.getElementById('results').innerHTML = `
-    <div class="funnelback-search-container">
-      ${getClearResponse || "No clear results found."}
-    </div>
-  `;
-}
+//   document.getElementById('results').innerHTML = `
+//     <div class="funnelback-search-container">
+//       ${getToolResponse || "No tool results found."}
+//     </div>
+//   `;
+// }
 
 
 
 
 // handle facet cleaners
-async function handlePagination(e) {
-  e.preventDefault();
+// async function handleClearFacet(e) {
+//   e.preventDefault();
 
-  // click could be on either a list item or directly on the anchor
-  const pagHref = 
-    e.target.getAttribute('href') ||
-    e.target.querySelector('a')?.getAttribute('href');
+//   const fetchClear = e.target.closest('a.facet-group__clear');
+//   const clearHref = fetchClear.getAttribute('href');
+//   console.log("Relative href:", clearHref);
 
-  // Fetch and process data using the relative link
-  let getPagResponse = null;
-  if (pagHref) {
-    try {
-      getPagResponse = await fetchFunnelbackResults(pagHref, 'GET');
-    } catch (error) {
-      console.error("Error fetching clear data:", error);
-      getPagResponse = "Error loading pagination results.";
-    }
-  }
+//   // Fetch and process data using the relative link
+//   let getClearResponse = null;
+//   if (clearHref) {
+//     try {
+//       getClearResponse = await fetchFunnelbackResults(clearHref, 'GET');
+//     } catch (error) {
+//       console.error("Error fetching clear data:", error);
+//       getClearResponse = "Error loading clear results.";
+//     }
+//   }
 
-  document.getElementById('results').innerHTML = `
-    <div class="funnelback-search-container">
-      ${getPagResponse || "No pagination results found."}
-    </div>
-  `;
-}
+//   document.getElementById('results').innerHTML = `
+//     <div class="funnelback-search-container">
+//       ${getClearResponse || "No clear results found."}
+//     </div>
+//   `;
+// }
+
+
+
+
+// handle facet cleaners
+// async function handlePagination(e) {
+//   e.preventDefault();
+
+//   // click could be on either a list item or directly on the anchor
+//   const pagHref = 
+//     e.target.getAttribute('href') ||
+//     e.target.querySelector('a')?.getAttribute('href');
+
+//   // Fetch and process data using the relative link
+//   let getPagResponse = null;
+//   if (pagHref) {
+//     try {
+//       getPagResponse = await fetchFunnelbackResults(pagHref, 'GET');
+//     } catch (error) {
+//       console.error("Error fetching clear data:", error);
+//       getPagResponse = "Error loading pagination results.";
+//     }
+//   }
+
+//   document.getElementById('results').innerHTML = `
+//     <div class="funnelback-search-container">
+//       ${getPagResponse || "No pagination results found."}
+//     </div>
+//   `;
+// }
 
 
 
 
 // handle spelling click
-async function handleSpellingClick(e, href) {
-  e.preventDefault();
+// async function handleSpellingClick(e, href) {
+//   e.preventDefault();
 
-  // Fetch and process data using the relative link
-  let getClickResponse = null;
-  if (href) {
-    try {
-      getClickResponse = await fetchFunnelbackSpelling(href, 'GET');
-    } catch (error) {
-      console.error("Error fetching clear data:", error);
-      getClickResponse = "Error click results.";
-    }
-  }
+//   // Fetch and process data using the relative link
+//   let getClickResponse = null;
+//   if (href) {
+//     try {
+//       getClickResponse = await fetchFunnelbackSpelling(href, 'GET');
+//     } catch (error) {
+//       console.error("Error fetching clear data:", error);
+//       getClickResponse = "Error click results.";
+//     }
+//   }
 
-  document.getElementById('results').innerHTML = `
-    <div class="funnelback-search-container">
-      ${getClickResponse || "No click results found."}
-    </div>
-  `;
-}
+//   document.getElementById('results').innerHTML = `
+//     <div class="funnelback-search-container">
+//       ${getClickResponse || "No click results found."}
+//     </div>
+//   `;
+// }
 
 
 
 
 // handle click
-async function handleClick(e, href) {
-  e.preventDefault();
+// async function handleClick(e, href) {
+//   e.preventDefault();
 
-  // Fetch and process data using the relative link
-  let getClickResponse = null;
-  if (href) {
-    try {
-      getClickResponse = await fetchFunnelbackResults(href, 'GET');
-    } catch (error) {
-      console.error("Error fetching clear data:", error);
-      getClickResponse = "Error click results.";
-    }
-  }
+//   // Fetch and process data using the relative link
+//   let getClickResponse = null;
+//   if (href) {
+//     try {
+//       getClickResponse = await fetchFunnelbackResults(href, 'GET');
+//     } catch (error) {
+//       console.error("Error fetching clear data:", error);
+//       getClickResponse = "Error click results.";
+//     }
+//   }
 
-  document.getElementById('results').innerHTML = `
-    <div class="funnelback-search-container">
-      ${getClickResponse || "No click results found."}
-    </div>
-  `;
-}
+//   document.getElementById('results').innerHTML = `
+//     <div class="funnelback-search-container">
+//       ${getClickResponse || "No click results found."}
+//     </div>
+//   `;
+// }
 
 
 
