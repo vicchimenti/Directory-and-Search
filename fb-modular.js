3f598be501f11c5ccfc297ad6a2ed1864852a23b
Vic Chimenti <victor.chimenti@gmail.com>
2024-12-16 09:09:31 -0800

    import modular template

---
+++ fb-modular.js
@@ -0,0 +1,37 @@
+class EventManager {
+    constructor(rootElement = document) {
+      this.rootElement = rootElement;
+      this.setupListeners();
+    }
+  
+    setupListeners() {
+      this.rootElement.addEventListener('click', this.handleClick.bind(this));
+    }
+  
+    handleClick(e) {
+      const target = e.target;
+  
+      const handlers = [
+        { 
+          selector: '.facet-group__list a', 
+          handler: () => handleFacetAnchor(e) 
+        },
+        { 
+          selector: '.tab-list__nav a', 
+          handler: () => handleTab(e) 
+        },
+        // Add other handlers...
+      ];
+  
+      for (const { selector, handler } of handlers) {
+        const matchedElement = target.closest(selector);
+        if (matchedElement) {
+          handler();
+          break;
+        }
+      }
+    }
+  }
+  
+  // Usage
+  const eventManager = new EventManager();
