8d8ebed8803d47c4f31ee848dd3f73ba1a6fccf7
Vic Chimenti <chimenti@seattleu.edu>
2025-01-01 08:24:48 -0800

    indents

--- pageLayout-su-website-search-test-8925251-footer.php
+++ pageLayout-su-website-search-test-8925251-footer.php
@@ -1,33 +1,33 @@
-</article>
-</main>
-</div>
-<!-- .page-wrapper -->
-<t4 type="navigation" name="Return Back to top" id="1083" />
-<t4 type="navigation" name="Return Sock" id="982" />
-<t4 type="navigation" name="Return Common Footer HTML" id="950" />
+                    </article>
+                </main>
+            </div>
+            <!-- .page-wrapper -->
+            <t4 type="navigation" name="Return Back to top" id="1083" />
+            <t4 type="navigation" name="Return Sock" id="982" />
+            <t4 type="navigation" name="Return Common Footer HTML" id="950" />
 
-</div>
-<!-- .page -->
-<script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
-<!-- jquery.min.js -->
-<script src="<t4 type='media' id='8723392' formatter='path/*' />" defer></script>
-<!-- flats.min.js -->
-<script src="<t4 type='media' id='6907524' formatter='path/*' />" defer></script>
-<!-- modernizr.js -->
-<script src="<t4 type='media' id='8718323' formatter='path/*' />" defer></script>
-<!-- app.min.js -->
-<script src="<t4 type='media' id='6907526' formatter='path/*' />" defer></script>
-<!-- touchevents.js -->
-<script src="<t4 type='media' id='6930621' formatter='path/*' />" defer></script>
-<!-- emergency-alert.js -->
-<script src="<t4 type='media' id='7022505' formatter='path/*' />" defer></script>
-<!-- t4-extras.js -->
-<t4 type="navigation" name="Return Gallery JS" id="956" />
-<t4 type="navigation" name="Return Event Promo JS" id="965" />
-<t4 type="navigation" name="Contact Listing JS" id="1029" />
-<t4 type="navigation" name="V10 - Custom Scripts" id="1015" />
-<t4 type="navigation" name="V10 Top Content Custom JS" id="1017" />
-<t4 type="navigation" name="Return Scripts for End of Body - Search" id="1090" />
+        </div>
+        <!-- .page -->
+        <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
+        <!-- jquery.min.js -->
+        <script src="<t4 type='media' id='8723392' formatter='path/*' />" defer></script>
+        <!-- flats.min.js -->
+        <script src="<t4 type='media' id='6907524' formatter='path/*' />" defer></script>
+        <!-- modernizr.js -->
+        <script src="<t4 type='media' id='8718323' formatter='path/*' />" defer></script>
+        <!-- app.min.js -->
+        <script src="<t4 type='media' id='6907526' formatter='path/*' />" defer></script>
+        <!-- touchevents.js -->
+        <script src="<t4 type='media' id='6930621' formatter='path/*' />" defer></script>
+        <!-- emergency-alert.js -->
+        <script src="<t4 type='media' id='7022505' formatter='path/*' />" defer></script>
+        <!-- t4-extras.js -->
+        <t4 type="navigation" name="Return Gallery JS" id="956" />
+        <t4 type="navigation" name="Return Event Promo JS" id="965" />
+        <t4 type="navigation" name="Contact Listing JS" id="1029" />
+        <t4 type="navigation" name="V10 - Custom Scripts" id="1015" />
+        <t4 type="navigation" name="V10 Top Content Custom JS" id="1017" />
+        <t4 type="navigation" name="Return Scripts for End of Body - Search" id="1090" />
 
-</body>
+    </body>
 </html>
