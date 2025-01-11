a493789e754bfdc72bc4db8c15aa5630a739abda
Vic Chimenti <victor.chimenti@gmail.com>
2025-01-11 13:11:50 -0800

    ud sotocomplete rm cart

--- pageLayout-su-website-search-test-8925251-footer.php
+++ pageLayout-su-website-search-test-8925251-footer.php
@@ -56,73 +56,21 @@
         <script src="https://dxp-us-search.funnelback.squiz.cloud/s/resources/seattleu~sp-search/_default/js/funnelback.autocompletion-2.6.0.js" defer></script>
         <!-- End Partial Scripts -->
         <script defer>
-            (function($) {
-                $('input.on-page-sq-search').autocompletion({
-                datasets: {
-                    organic: {
-                    collection: 'seattleu~sp-search',
-                    profile   : '_default',
-                    program   : 'https://dxp-us-search.funnelback.squiz.cloud/s/suggest.json',
-                    }
-                },
-                length: 3
-                });
-            })(jQuery);
-        </script>
-        <script defer>
-            window.addEventListener('DOMContentLoaded', function() {	
-                new Funnelback.SessionCart({
-                    apiBase: 'https://dxp-us-stage-search.funnelback.squiz.cloud/s/cart.json',
-                    collection: 'seattleu~sp-search',
-                    iconPrefix: '',
-                    cartCount: {
-                        template: '{{>icon-block}} {{>label-block}} ({{count}})',
-                        icon: 'fas fa-star',
-                        label: 'Shortlist',
-                        isLabel: true,
-                        classes: "",
-                        enableClick: false,
-                        elementType: "span",
-                        tabIndex: "-1",
-                    },
-                    cart: {
-                        icon: '',
-                        label: '',
-                        backIcon: 'fas fa-arrow-left',
-                        backLabel: 'Back to results',
-                        clearIcon: 'fas fa-times',
-                        clearClasses: "btn btn-xs btn-light",                    
-                        emptyMessage: '<span id="flb-cart-empty-message">No items in your shortlist</span>',
-                    },
-                    item: {
-                        icon: 'fas fa-star',          
-                        templates: {
-                            'default': document.getElementById('shortlist-template-default').text,
-                            'seattleu~ds-programs': document.getElementById('shortlist-template-programs').text
-                        },
-                        class: ''
-                    },
-                    resultItemTrigger: {
-                        selector: '.enable-cart-on-result',
-                        class: '',
-                        labelAdd: 'ADD TO SHORTLIST',
-                        iconAdd: 'far fa-star',
-                        labelDelete: 'REMOVE FROM SHORTLIST',
-                        iconDelete: 'fas fa-star',
-                        isLabel: true,
-                        template: '{{>icon-block}} {{>label-block}}',
-                        position: 'afterbegin'
-                    },
-                    /* Trigger to delete an item from the cart */
-                    cartItemTrigger: {
-                        selector: ".fb-cart__remove",
-                        iconDelete: "fas",
-                        template: '{{>icon-block}}{{>label-block}}',
-                        position: 'afterbegin',
-                        isLabel: true,
-                        labelDelete: "REMOVE FROM SHORTLIST"
-                    }        
-                });
+            $('#input.on-page-sq-search').autocompletion({
+            alpha: '0.5',
+            show: '10',
+            sort: '0',
+            length: '3',
+            datasets: {
+                organic: {
+                name: 'Suggestions',
+                collection: 'seattleu~sp-search',
+                profile   : '_default',
+                program   : 'https://seattleu~sp-search.clients.us.funnelback.com/s/suggest.json',
+                show: 10,
+                }
+            },
+            length: 3
             });
         </script>
 
