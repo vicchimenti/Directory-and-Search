492312b182dfb8a4170b5ea8ffebd2e958d42930
Vic Chimenti <victor.chimenti@gmail.com>
2025-01-12 12:28:49 -0800

    rm ip address functionality

--- header-search-manager.js
+++ header-search-manager.js
@@ -1,24 +1,12 @@
 class HeaderSearchManager {
     constructor() {
         this.setupHeaderSearch();
-        this.initializeIP();
     }
 
     setupHeaderSearch() {
         const searchBar = document.getElementById("search-button");
         if (searchBar) {
             searchBar.addEventListener('click', this.handleHeaderSearch);
-        }
-    }
-
-    async initializeIP() {
-        try {
-            let response = await fetch('https://api.ipify.org?format=json');
-            let data = await response.json();
-            this.userIp = data.ip;
-        } catch (error) {
-            console.error('Error fetching IP address:', error);
-            this.userIp = '';
         }
     }
 
@@ -30,10 +18,6 @@ class HeaderSearchManager {
             alert('Please enter a search term');
             return;
         }
-
-        // Add the IP to the request headers
-        const headers = new Headers();
-        headers.append('X-Forwarded-For', this.userIp);
 
         const redirectUrl = `/search-test/?query=${encodeURIComponent(searchQuery)}&collection=seattleu~sp-search&profile=_default`;
         window.location.href = redirectUrl;
