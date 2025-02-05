e86064c9d7415d805e341c1f08d8dc2c501c3791
Vic Chimenti <chimenti@seattleu.edu>
2025-02-05 06:45:10 -0800

    udv

--- results-search-manager.js
+++ results-search-manager.js
@@ -22,8 +22,8 @@
  * - header-search-manager.js: Handles initial search and redirects
  * 
  * @author Victor Chimenti
- * @version 1.3.0
- * @lastModified 2025-02-04
+ * @version 1.3.1
+ * @lastModified 2025-02-05
  */
 
 import DOMObserverManager from './dom-observer-manager.js';
