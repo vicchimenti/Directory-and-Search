6d7441948cdc2376f331b6cb18b7507eb7f53f33
Vic Chimenti <victor.chimenti@gmail.com>
2025-02-05 10:37:44 -0800

    udv

--- dynamic-results-manager.js
+++ dynamic-results-manager.js
@@ -28,7 +28,7 @@
 * - header-search-manager.js: Handles initial search and redirects
 * 
 * @author Victor Chimenti
-* @version 1.3.1
+* @version 1.3.2
 * @lastModified 2025-02-05
 */
 import DOMObserverManager from './dom-observer-manager.js';
