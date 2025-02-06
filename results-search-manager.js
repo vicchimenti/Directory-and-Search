12f36d5135b6b5872683dd3014d4ff4327d98ed1
Vic Chimenti <chimenti@seattleu.edu>
2025-02-06 06:22:48 -0800

    btn id on-page-search-button

--- results-search-manager.js
+++ results-search-manager.js
@@ -48,7 +48,7 @@ class ResultsSearchManager {
      */
     #setupResultsSearch() {
         console.log("setupResultsSearch");
-        const onPageSearch = document.getElementById("autocomplete-concierge-inputField");
+        const onPageSearch = document.getElementById("on-page-search-button");
         if (onPageSearch) {
             onPageSearch.addEventListener('click', this.#handleResultsSearch);
         } else {
