23e9e39abe3cb51186654fbc80202844e6ba8215
Vic Chimenti <victor.chimenti@gmail.com>
2025-03-10 15:43:24 -0700

    page layout header ready

--- Launch Assets/pageLayout-su-web-2023-6907517-header.php
+++ Launch Assets/pageLayout-su-web-2023-6907517-header.php
@@ -23,21 +23,17 @@
     <meta name="Language" content="en-US">
     <meta name="googlebot" content="all">
     <meta name="robots" content="all">
-    
 
     <!-- Preconnects -->
-
     <link rel="preconnect" href="https://fonts.googleapis.com"  />
 
     <!-- Preloads -->
-
     <link
         rel="preload"
         href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,500;0,600;0,700;1,500;1,800&amp;family=Oswald:wght@300;700&amp;family=Roboto+Slab:wght@200;300&amp;display=swap"
         as="style"
         crossorigin
     />
-
     <link
         rel="preload"
         href="/media/seattle-university/site-assets/css/theme.min.css"
@@ -45,12 +41,9 @@
     />
 
     <!-- Custom Head Code -->
-
     <t4 type="navigation" name="V10 - Custom Header Code" id="1014" />
-
     
     <!-- Nav Icons -->
-
     <link rel="apple-touch-icon" sizes="180x180" href="<t4 type="media" id="6913741" formatter="path/*" />">
     <link rel="icon" type="image/png" sizes="32x32" href="<t4 type="media" id="6913745" formatter="path/*" />">
     <link rel="icon" type="image/png" sizes="16x16" href="<t4 type="media" id="6913749" formatter="path/*" />">
@@ -59,9 +52,7 @@
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="msapplication-config" content="<t4 type="media" id="6913806" formatter="path/*" />">
 
-    
     <!-- Stylesheets -->
-
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,500;0,600;0,700;1,500;1,800&amp;family=Oswald:wght@300;700&amp;family=Roboto+Slab:wght@200;300&amp;display=swap" />
     <t4 type="media" id="6907520" formatter="cssver/*" />
     <!-- app.css -->
@@ -74,12 +65,9 @@
     <t4 type="media" id="8953952" formatter="cssver/*" />
     <!-- funnelback.css -->
 
-    
     <!-- Custom -->
-
     <t4 type="navigation" name="Return Scripts for Head" id="978" />
     <t4 type="navigation" name="V10 - Custom CSS Zone" id="1013" />
-
 
 </head>
 
@@ -87,7 +75,7 @@
 
     <t4 type="navigation" name="Return Scripts for Start of Body" id="979" />
 
-    <div class="page page--page <t4 type="navigation" name="Return Page Class" id="966" />">
+    <div class="page page--page <t4 type='navigation' name='Return Page Class' id='966' />">
 
         <t4 type="navigation" name="Return Site Wide Notices" id="970" />
 
