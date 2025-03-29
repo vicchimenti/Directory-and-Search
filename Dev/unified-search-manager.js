0d4d809df7ea720740b4e7c2e93b43f7a997bba8
Vic Chimenti <chimenti@seattleu.edu>
2025-03-29 06:01:02 -0700

    chore(class level jsdocs): move out of class

--- Dev/unified-search-manager.js
+++ Dev/unified-search-manager.js
@@ -21,7 +21,7 @@
  * 
  * @author Victor Chimenti
  * @version 5.0.0
- * @devVersion 2.3.2
+ * @devVersion 2.3.3
  * @prodVersion 4.0.0
  * @environment development
  * @status in-progress
@@ -29,13 +29,13 @@
  * @lastModified 2025-03-29
  */
 
+/**
+ * Initializes the Unified Search Manager.
+ * Finds and initializes all search components on the page.
+ * 
+ * @param {Object} [config={}] - Configuration options
+ */
 class UnifiedSearchManager {
-    /**
-     * Initializes the Unified Search Manager.
-     * Finds and initializes all search components on the page.
-     * 
-     * @param {Object} [config={}] - Configuration options
-     */
     constructor(config = {}) {
         try {
             console.log('Starting UnifiedSearchManager constructor');
