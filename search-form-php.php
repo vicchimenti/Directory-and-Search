9fcaf8c1f52a524a024ec24214b882503ea66f39
Vic Chimenti <victor.chimenti@gmail.com>
2025-05-09 14:56:57 -0700

    import staff search form

--- search-form-php.php
+++ search-form-php.php
@@ -1,5 +1,4 @@
 <?php
-
 $genericFacet = \T4\PHPSearchLibrary\FacetFactory::getInstance('GenericFacet', $documentCollection, $queryHandler);
 $filters = $queryHandler->getQueryValuesForPrint();
 $categoryFilters = array('typeOfStaff', 'staffDepartment', 'schoolCollege');
@@ -70,56 +69,51 @@ $i = 0;
     <div class="filter-feedback">
         <div class="grid-container">
             <span id="starthere"></span>
-            <div id="searchoptions-filters" class="active-filters" role="search" data-t4-ajax-group="courseSearch" aria-label="Deselect Filters">
-                <div id="event-filters" class="active-filters--list" >
-                    <span>Active filters:</span>
-                    <?php if ($filters !== null) : ?>
-                        <ul class="no-bullet">
+                <div id="searchoptions-filters" class="active-filters" role="search" data-t4-ajax-group="courseSearch" aria-label="Deselect Filters">
+                    <div id="event-filters" class="active-filters--list" >
+                      
+                        <span><?php if ($queryHandler->doQuerysExist()): ?>Active filters:<?php endif; ?></span>
+                        <?php if ($filters !== null) : ?>
                             <?php
+                            $tagsHTML = '';
                             foreach ($categoryFilters as $key) {
                                 if (isset($filters[$key]) && is_array($filters[$key])) :
-                                    foreach ($filters[$key] as $value) : ?>
-                                        <li class="filter-<?php echo $i++ ?>  small primary" tabindex="0" data-t4-value="<?php echo strtolower($value) ?>" data-t4-filter="<?php echo $key ?>"><?php echo $value ?><span class="remove"><i class="fa fa-times"></i></span></li>
-                                    <?php
+                                    foreach ($filters[$key] as $value) : 
+                                        $tagsHTML .= '<li class="filter-' . $i++ . ' small primary" role="button" tabindex="0" data-t4-value="' . strtolower($value) . '" data-t4-filter="' . $key . '">' . $value . '<span class="remove"><i class="fa fa-times"></i></span></li>'; 
                                     endforeach;
                                 elseif (isset($filters[$key])) :
-                                    $value = $filters[$key]; ?>
-                                    <li class="filter-<?php echo $i++ ?>  small primary" tabindex="0" data-t4-value="<?php echo strtolower($value) ?>" data-t4-filter="<?php echo $key ?>"><?php echo $value ?><span class="remove"><i class="fa fa-times"></i></span></li>
-                                <?php
+                                    $value = $filters[$key];
+                                    $tagsHTML .= '<li class="filter-' . $i++ . ' small primary" role="button" tabindex="0" data-t4-value="' . strtolower($value) . '" data-t4-filter="' . $key . '">' . $value . '<span class="remove"><i class="fa fa-times"></i></span></li>'; 
                                 endif;
                             }
                             foreach ($dateFilters as $key) {
                                 if (isset($filters[$key])) :
-                                    $value = $filters[$key]; ?>
-                                    <li class="filter-<?php echo $i++ ?>  small primary" tabindex="0" data-t4-filter="<?php echo $key ?>"><?php echo date('m/d/Y', strtotime($value)); ?><span class="remove"><i class="fa fa-times"></i></span></li>
-                                <?php
+                                    $value = $filters[$key]; 
+                                    $tagsHTML .= '<li class="filter-' . $i++ . ' small primary" role="button" tabindex="0" data-t4-filter="' . $key . '">' . date('m/d/Y', strtotime($value)) . '<span class="remove"><i class="fa fa-times"></i></span></li>'; 
                                 endif;
                             }
                             foreach ($rangeFilters as $key => $max) {
                                 if (isset($filters[$key]) && $filters[$key] !== $max) :
-                                    $value = $filters[$key]; ?>
-                                    <li class="filter-<?php echo $i++ ?>  small primary" tabindex="0" data-t4-filter="<?php echo $key ?>"><?php echo '$' . $value; ?><span class="remove"><i class="fa fa-times"></i></span></li>
-                                <?php
+                                    $value = $filters[$key]; 
+                                    $tagsHTML .= '<li class="filter-' . $i++ . ' small primary" role="button" tabindex="0" data-t4-filter="' . $key . '">$' . $value . '<span class="remove"><i class="fa fa-times"></i></span></li>'; 
                                 endif;
                             }
                             if (isset($filters['keywords'])) :
-                                ?>
-                                <li class="filter-<?php echo $i++ ?> small primary" tabindex="0" data-t4-filter="keywords"> <?php echo $filters['keywords'] ?><span class="remove"><i class="fa fa-times"></i></span></li>
-                            <?php
-                            endif; ?>
-                        </ul>
-                    <?php endif; ?>
+                                $tagsHTML .= '<li class="filter-' . $i++ . ' small primary" role="button" tabindex="0" data-t4-filter="keywords">' . $filters['keywords'] . '<span class="remove"><i class="fa fa-times"></i></span></li>'; 
+                            endif; 
+                            echo $tagsHTML != '' ? '<ul class="no-bullet">' . $tagsHTML . '</ul>' : '';
+                            ?>
+                        <?php endif; ?>
+                    </div>
+                <?php if ($i > 0) : ?>
+                <div class="funderline">
+                    <a href="index.php" role="button" data-t4-ajax-link="true">
+                    Clear Filters
+                    <span class="fa fa-times"></span>
+                    </a>
                 </div>
-              <?php if ($i > 0) : ?>
-              <div class="funderline">
-                <a href="index.php" role="button" data-t4-ajax-link="true">
-                  Clear Filters
-                  <span class="fa fa-times"></span>
-                </a>
-              </div>
-              <?php endif; ?>   
-              <div class="search-count"><p>Showing <strong><?php echo count($results); ?> articles</strong> of <?php echo $totalResults; ?></p></div>
-            </div>
+                <?php endif; ?>  
+                </div>
         </div>
     </div>
 </section>
